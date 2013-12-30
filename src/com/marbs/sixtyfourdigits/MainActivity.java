0d9db72fc6121a278a5901d225e4bb38eb1a020b
marbsydo <muffin@Muffins-MacBook-Pro.local>
2013-12-30 22:18:24 +0000

    Fixed code formatting and indentation

--- src/com/marbs/sixtyfourdigits/MainActivity.java
+++ src/com/marbs/sixtyfourdigits/MainActivity.java
@@ -32,10 +32,10 @@ import com.squareup.picasso.Picasso;
 public class MainActivity extends Activity {
 
 	public final static String SITE_PATH = "http://www.64digits.com/index.php?id=0&cmd=&page=0";
-	
+
 	ProgressDialog pd;
 	public Context context = this;
-	
+
 	// Data for each item on the front page
 	public class FrontPageItemData {
 		String imageUrl;
@@ -43,199 +43,224 @@ public class MainActivity extends Activity {
 		String excerpt;
 		String author;
 		int numComments;
-		
-		public FrontPageItemData(String title, String author, int numComments, String imageUrl) {
+
+		public FrontPageItemData(String title, String author, int numComments,
+				String imageUrl) {
 			this.title = title;
 			this.author = author;
 			this.numComments = numComments;
 			this.imageUrl = imageUrl;
 		}
-		
+
 		public String GetTitle() {
 			return title;
 		}
-		
+
 		public String GetAuthor() {
 			return author;
 		}
-		
+
 		public int GetNumComments() {
 			return numComments;
 		}
-		
+
 		public String GetImageUrl() {
 			return imageUrl;
 		}
 	}
-	
-	  @Override
-	  protected void onCreate(Bundle savedInstanceState) {
-	    super.onCreate(savedInstanceState);
-	    setContentView(R.layout.activity_main);
-	    
-	    new FrontPage().execute();
-	  }
 
-	  private class FrontPageItemAdapter extends ArrayAdapter<FrontPageItemData> {
-		  
-		  private class ViewHolder {
-			  private TextView textViewTitle;
-			  private TextView textViewAuthor;
-			  private ImageView imageViewAvatar;
-			  
-			  public ViewHolder() {
-				  // Do nothing
-			  }
-		  }
-		  
-		  private final LayoutInflater inflater;
-		  
-		  public FrontPageItemAdapter(Context context, int textViewResourceId, List<FrontPageItemData> itemData) {
-			  super(context, textViewResourceId, itemData);
-			  
-			  this.inflater = LayoutInflater.from(context);
-		  }
-		  
-		  @Override
-		  public View getView(final int position, final View convertView, final ViewGroup parent) {
-			  View itemView = convertView;
-			  ViewHolder holder = null;
-			  final FrontPageItemData item = getItem(position);
-			  if (null == itemView) {
-				  itemView = this.inflater.inflate(R.layout.frontpage_item, parent, false);
-				  
-				  holder = new ViewHolder();
-				  
-				  holder.textViewTitle = (TextView)itemView.findViewById(R.id.textTitle);
-				  holder.textViewAuthor = (TextView)itemView.findViewById(R.id.textAuthor);
-				  holder.imageViewAvatar = (ImageView)itemView.findViewById(R.id.imageAvatar);
-				  
-				  itemView.setTag(holder);
-			  } else {
-				  holder = (ViewHolder)itemView.getTag();
-			  }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-			  // Set the title text
-			  holder.textViewTitle.setText(item.GetTitle());
-			  
-			  // Set the author and number of comments text
-			  int num = item.GetNumComments();
-			  holder.textViewAuthor.setText(item.GetAuthor() + " (" + num + " comment" + (num == 1 ? "" : "s") + ")");
-			  
-			  // Tell Picasso to load the avatar into the image view
-			  Picasso.with(context).load(item.GetImageUrl()).into(holder.imageViewAvatar);
-			  
-			  return itemView;
-		  }
-	  }
-
-	    private class FrontPage extends AsyncTask<Void, Void, Void> {
-
-	    	ArrayList<FrontPageItemData> frontPageData;
-	    	
-	    	@Override
-	    	protected void onPreExecute() {
-	    		super.onPreExecute();
-	    		pd = new ProgressDialog(MainActivity.this);
-	    		pd.setTitle("Retrieve 64D Front Page");
-	    		pd.setMessage("Loading...");
-	    		pd.setIndeterminate(false);
-	    		pd.show();
-	    	}
-	    	
-	    	@Override
-	    	protected Void doInBackground(Void... params) {
-
-	    		frontPageData = new ArrayList<FrontPageItemData>();
-	    	    
-	    		boolean errorOccurred = false;
-	    		String errorString = "";
-	    		
-	    		Connection.Response response;
-	    		try {
-	    			response = Jsoup.connect(SITE_PATH)
-							.userAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.21 (KHTML, like Gecko) Chrome/19.0.1042.0 Safari/535.21")
-							.timeout(10000)
-							.execute();
-				} catch (IOException e) {
-					response = null;
-					System.out.println("Error: could not connect");
-					errorOccurred = true;
-					errorString = "Could not connect";
-					e.printStackTrace();
-				}
-	    		
-	    		if (response != null) {
-	    			int statusCode = response.statusCode();
-	    			if (statusCode == 200) { // OK
-	    				Document doc;
-	    				try {
-							doc = response.parse();
-						} catch (IOException e) {
-							doc = null;
-							System.out.println("Error: Could not parse data");
-							errorOccurred = true;
-							errorString = "Could not parse data";
-							e.printStackTrace();
-						}
-	    				
-	    				if (doc != null) {
-	    	    			Elements frontPageBlogs = doc.select("div.middlecontent div.fnt11.fntgrey");
-	    	    			for (Element blog : frontPageBlogs) {
-	    	    				try {
-	    	    					// Title
-	    	    					String title = blog.select("a.lnknodec.fntblue.fntbold.fnt15").first().text();
-	    	    					
-	    	    					// Author
-	    	    					String author = blog.select("a.fntblue").get(1).text();
-	    	    					
-	    	    					// Number of comments
-	    	    					String numCommentsString = blog.select("a.fntblue").get(2).text();
-	    	    					int numComments = -1;
-	    	    					Pattern p = Pattern.compile("\\d+");
-	    	    					Matcher m = p.matcher(numCommentsString);
-	    	    					if (m.find()) {
-	    	    						numComments = Integer.parseInt(m.group());
-	    	    					}
-	    	    					
-	    	    					// Image URL
-	    	    					String imageUrl = blog.select("img").first().absUrl("src");
-	    	    					// Encode the URL so that e.g. spaces become %20s
-	    	    					URL url = new URL(imageUrl);
-	    	    					URI uri = new URI(url.getProtocol(), url.getUserInfo(), url.getHost(), url.getPort(), url.getPath(), url.getQuery(), url.getRef());
-	    	    					imageUrl = uri.toURL().toString();
-	    	    					
-	    	    					frontPageData.add(new FrontPageItemData(title, author, numComments, imageUrl));
-	    	    				} catch (Exception e) {
-	    	    					System.out.println("Error: Selecting threw error: " + e);
-	    	    					errorOccurred = true;
-	    	    					errorString = "Could not select parsed data";
-	    	    				}
-	    	    			}
-	    				}
-	    			} else {
-	    				System.out.println("Error: Received status code: " + statusCode);
-						errorOccurred = true;
-						errorString = "Received status code " + statusCode;
-	    			}
-	    		}
-	    		
-	    		if (errorOccurred) {
-	    			frontPageData.add(new FrontPageItemData("Error!", errorString, -1, ""));
-	    		}
-
-	    		return null;
-	    	}
-	    	
-	    	@Override
-	    	protected void onPostExecute(Void result) {
-
-	    		final ListView listview = (ListView) findViewById(R.id.listview);
-	    		final FrontPageItemAdapter adapter = new FrontPageItemAdapter(context, R.layout.frontpage_item, frontPageData);
-	    		listview.setAdapter(adapter);
-	    		
-	    	    pd.dismiss();
-	    	}
-	    }
-	  
+		new FrontPage().execute();
 	}
+
+	private class FrontPageItemAdapter extends ArrayAdapter<FrontPageItemData> {
+
+		private class ViewHolder {
+			private TextView textViewTitle;
+			private TextView textViewAuthor;
+			private ImageView imageViewAvatar;
+
+			public ViewHolder() {
+				// Do nothing
+			}
+		}
+
+		private final LayoutInflater inflater;
+
+		public FrontPageItemAdapter(Context context, int textViewResourceId,
+				List<FrontPageItemData> itemData) {
+			super(context, textViewResourceId, itemData);
+
+			this.inflater = LayoutInflater.from(context);
+		}
+
+		@Override
+		public View getView(final int position, final View convertView,
+				final ViewGroup parent) {
+			View itemView = convertView;
+			ViewHolder holder = null;
+			final FrontPageItemData item = getItem(position);
+			if (null == itemView) {
+				itemView = this.inflater.inflate(R.layout.frontpage_item,
+						parent, false);
+
+				holder = new ViewHolder();
+
+				holder.textViewTitle = (TextView) itemView
+						.findViewById(R.id.textTitle);
+				holder.textViewAuthor = (TextView) itemView
+						.findViewById(R.id.textAuthor);
+				holder.imageViewAvatar = (ImageView) itemView
+						.findViewById(R.id.imageAvatar);
+
+				itemView.setTag(holder);
+			} else {
+				holder = (ViewHolder) itemView.getTag();
+			}
+
+			// Set the title text
+			holder.textViewTitle.setText(item.GetTitle());
+
+			// Set the author and number of comments text
+			int num = item.GetNumComments();
+			holder.textViewAuthor.setText(item.GetAuthor() + " (" + num
+					+ " comment" + (num == 1 ? "" : "s") + ")");
+
+			// Tell Picasso to load the avatar into the image view
+			Picasso.with(context).load(item.GetImageUrl())
+					.into(holder.imageViewAvatar);
+
+			return itemView;
+		}
+	}
+
+	private class FrontPage extends AsyncTask<Void, Void, Void> {
+
+		ArrayList<FrontPageItemData> frontPageData;
+
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+			pd = new ProgressDialog(MainActivity.this);
+			pd.setTitle("Retrieve 64D Front Page");
+			pd.setMessage("Loading...");
+			pd.setIndeterminate(false);
+			pd.show();
+		}
+
+		@Override
+		protected Void doInBackground(Void... params) {
+
+			frontPageData = new ArrayList<FrontPageItemData>();
+
+			boolean errorOccurred = false;
+			String errorString = "";
+
+			Connection.Response response;
+			try {
+				response = Jsoup
+						.connect(SITE_PATH)
+						.userAgent(
+								"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.21 (KHTML, like Gecko) Chrome/19.0.1042.0 Safari/535.21")
+						.timeout(10000).execute();
+			} catch (IOException e) {
+				response = null;
+				System.out.println("Error: could not connect");
+				errorOccurred = true;
+				errorString = "Could not connect";
+				e.printStackTrace();
+			}
+
+			if (response != null) {
+				int statusCode = response.statusCode();
+				if (statusCode == 200) { // OK
+					Document doc;
+					try {
+						doc = response.parse();
+					} catch (IOException e) {
+						doc = null;
+						System.out.println("Error: Could not parse data");
+						errorOccurred = true;
+						errorString = "Could not parse data";
+						e.printStackTrace();
+					}
+
+					if (doc != null) {
+						Elements frontPageBlogs = doc
+								.select("div.middlecontent div.fnt11.fntgrey");
+						for (Element blog : frontPageBlogs) {
+							try {
+								// Title
+								String title = blog
+										.select("a.lnknodec.fntblue.fntbold.fnt15")
+										.first().text();
+
+								// Author
+								String author = blog.select("a.fntblue").get(1)
+										.text();
+
+								// Number of comments
+								String numCommentsString = blog
+										.select("a.fntblue").get(2).text();
+								int numComments = -1;
+								Pattern p = Pattern.compile("\\d+");
+								Matcher m = p.matcher(numCommentsString);
+								if (m.find()) {
+									numComments = Integer.parseInt(m.group());
+								}
+
+								// Image URL
+								String imageUrl = blog.select("img").first()
+										.absUrl("src");
+								// Encode the URL so that e.g. spaces become
+								// %20s
+								URL url = new URL(imageUrl);
+								URI uri = new URI(url.getProtocol(),
+										url.getUserInfo(), url.getHost(),
+										url.getPort(), url.getPath(),
+										url.getQuery(), url.getRef());
+								imageUrl = uri.toURL().toString();
+
+								frontPageData.add(new FrontPageItemData(title,
+										author, numComments, imageUrl));
+							} catch (Exception e) {
+								System.out
+										.println("Error: Selecting threw error: "
+												+ e);
+								errorOccurred = true;
+								errorString = "Could not select parsed data";
+							}
+						}
+					}
+				} else {
+					System.out.println("Error: Received status code: "
+							+ statusCode);
+					errorOccurred = true;
+					errorString = "Received status code " + statusCode;
+				}
+			}
+
+			if (errorOccurred) {
+				frontPageData.add(new FrontPageItemData("Error!", errorString,
+						-1, ""));
+			}
+
+			return null;
+		}
+
+		@Override
+		protected void onPostExecute(Void result) {
+
+			final ListView listview = (ListView) findViewById(R.id.listview);
+			final FrontPageItemAdapter adapter = new FrontPageItemAdapter(
+					context, R.layout.frontpage_item, frontPageData);
+			listview.setAdapter(adapter);
+
+			pd.dismiss();
+		}
+	}
+}
