8e5700fdd68ca4e3090cabd6b837c8d8747db0de
marbsydo <muffin@Muffins-MacBook-Pro.local>
2013-12-30 12:03:13 +0000

    Displays a messy list of the front page

--- src/com/marbs/sixtyfourdigits/MainActivity.java
+++ src/com/marbs/sixtyfourdigits/MainActivity.java
@@ -1,59 +1,34 @@
 package com.marbs.sixtyfourdigits;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class MainActivity extends Activity {
 
+	ProgressDialog pd;
+	Context context = this;
+	
 	  @Override
 	  protected void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
 	    setContentView(R.layout.activity_main);
-
-	    final ListView listview = (ListView) findViewById(R.id.listview);
-	    String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
-	        "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
-	        "Linux", "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux",
-	        "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux", "OS/2",
-	        "Android", "iPhone", "WindowsMobile" };
-
-	    final ArrayList<String> list = new ArrayList<String>();
-	    for (int i = 0; i < values.length; ++i) {
-	      list.add(values[i]);
-	    }
-	    final StableArrayAdapter adapter = new StableArrayAdapter(this,
-	        android.R.layout.simple_list_item_1, list);
-	    listview.setAdapter(adapter);
-
-	    listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-	      @Override
-	      public void onItemClick(AdapterView<?> parent, final View view,
-	          int position, long id) {
-	    	  /*
-	        final String item = (String) parent.getItemAtPosition(position);
-	        view.animate().setDuration(2000).alpha(0)
-	            .withEndAction(new Runnable() {
-	              @Override
-	              public void run() {
-	                list.remove(item);
-	                adapter.notifyDataSetChanged();
-	                view.setAlpha(1);
-	              }
-	            });
-	        */
-	      }
-
-	    });
+	    
+	    new FrontPage().execute();
 	  }
 
 	  private class StableArrayAdapter extends ArrayAdapter<String> {
@@ -81,6 +56,84 @@ public class MainActivity extends Activity {
 
 	  }
 
+	    private class FrontPage extends AsyncTask<Void, Void, Void> {
+	    	
+	    	ArrayList<String> list;
+	    	
+	    	@Override
+	    	protected void onPreExecute() {
+	    		super.onPreExecute();
+	    		pd = new ProgressDialog(MainActivity.this);
+	    		pd.setTitle("Retrieve 64D Front Page");
+	    		pd.setMessage("Loading...");
+	    		pd.setIndeterminate(false);
+	    		pd.show();
+	    	}
+	    	
+	    	@Override
+	    	protected Void doInBackground(Void... params) {
+	    	    
+	    	    
+	    	    list = new ArrayList<String>();
+	    	    
+	    		try {
+	    			Document doc = Jsoup.connect("http://www.64digits.com").timeout(3000).get();
+	    			String title = doc.title();
+	    			list.add(title);
+	    			Elements descElements = doc.select("div.fntgrey");
+	    			for (Element desc : descElements) {
+	    				list.add(desc.toString());
+	    			}
+	    		} catch (IOException e) {
+	    			e.printStackTrace();
+	    		}
+
+	    	    /*
+	    	    final StableArrayAdapter adapter = new StableArrayAdapter(context,
+	    	    		android.R.layout.simple_list_item_1, list);
+	    	    listview.setAdapter(adapter);
+	    	    */
+	    		
+	    	    /*
+	    listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+	      @Override
+	      public void onItemClick(AdapterView<?> parent, final View view,
+	          int position, long id) {
+	        final String item = (String) parent.getItemAtPosition(position);
+	        view.animate().setDuration(2000).alpha(0)
+	            .withEndAction(new Runnable() {
+	              @Override
+	              public void run() {
+	                list.remove(item);
+	                adapter.notifyDataSetChanged();
+	                view.setAlpha(1);
+	              }
+	            });
+	      }
+
+	    });
+	    	     */
+	    		return null;
+	    	}
+	    	
+	    	@Override
+	    	protected void onPostExecute(Void result) {
+	    		/*
+	    		Button titleButton = (Button) findViewById(R.id.get_title);
+	    		titleButton.setText(title);
+	    		EditText editText = (EditText) findViewById(R.id.edit_message);
+	    		editText.setText(desc);
+	    		*/
+	    		final ListView listview = (ListView) findViewById(R.id.listview);
+	    	    final StableArrayAdapter adapter = new StableArrayAdapter(context,
+	    	    		android.R.layout.simple_list_item_1, list);
+	    	    listview.setAdapter(adapter);
+	    	    
+	    	    pd.dismiss();
+	    	}
+	    }
+	  
 	} 
 
 /*
